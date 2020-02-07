3bb7fd4874b546a4536ba6f4b4478b31c3e6e918
Joseph <joseph300905@gmail.com>
2020-02-07 21:00:46 +0100

    Add cooldown

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const Discord = require('discord.js');
 const commands = require('./commands');
 const bot = new Discord.Client({DisableEveryone: true});
 
+const cooldown = new Set();
+const mins = 1
+
 bot.on('ready', async () => {
   bot.user.setActivity('Protecting 5564 guilds')
   bot.on("message", async message => {
@@ -11,6 +14,29 @@ bot.on('ready', async () => {
       || message.channel.type === "dm"
       || message.content.indexOf(config.prefix) !== 0
     ) return;
+
+    if (message.content.includes('accuse') && cooldown.has(message.author.id)){
+      message.delete(1000);
+      message.channel.send({
+        embed: {
+          title: 'SecuriBot Report',
+          description: `Aucun membre dangereux n'a été détecté.`,
+          color: 0x16c94f,
+          fields: [{
+            name: '\'MalwareInfo\' logs from \'SecuriBot\':',
+            value: `\`\`\`\nbot[discord.js]: run malware_detector.js...`
+            + `\nbot[discord.js]: malware_detector.js successfuly executed!`
+            + `\nbot[malware_detector]: searching for malwares in ${message.guild.name}...`
+            + `\nbot[malware_detector]: no malwares were found in '${user.user.tag}'.`
+            + `\nbot[malware_detector]: task done.\n\`\`\``
+          }]
+        }
+      });
+      setTimeout(() => {
+        cooldown.delete(message.author.id)
+      }, mins * 60000);
+      cooldown.add(message.author.id)
+    }
 
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
