ca22b56fdf72ea00e8d1b01f08330bfa72382e13
Joseph <joseph300905@gmail.com>
2020-02-07 21:17:00 +0100

    Remove timer

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ bot.on('ready', async () => {
       || message.content.indexOf(config.prefix) !== 0
     ) return;
 
-    if (message.content.includes('accuse') && cooldown.has(message.author.id)){
-      message.delete(1000);
-      message.channel.send({
-        embed: {
-          title: 'SecuriBot Report',
-          description: `Aucun membre dangereux n'a été détecté.`,
-          color: 0x16c94f,
-          fields: [{
-            name: '\'MalwareInfo\' logs from \'SecuriBot\':',
-            value: `\`\`\`\nbot[discord.js]: run malware_detector.js...`
-            + `\nbot[discord.js]: malware_detector.js successfuly executed!`
-            + `\nbot[malware_detector]: searching for malwares in ${message.guild.name}...`
-            + `\nbot[malware_detector]: no malwares were found in '${user.user.tag}'.`
-            + `\nbot[malware_detector]: task done.\n\`\`\``
-          }]
-        }
-      });
-      setTimeout(() => {
-        cooldown.delete(message.author.id)
-      }, mins * 60000);
-      cooldown.add(message.author.id)
-    }
-
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
