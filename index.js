6e716584f43a48eb1cf716a8ccd66facea447d9e
Joseph <joseph300905@gmail.com>
2020-02-07 18:08:52 +0100

    Fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require('./config.js');
 const Discord = require('discord.js');
+const commands = require('./commands');
 const bot = new Discord.Client({DisableEveryone: true});
 
 bot.on('ready', async () => {
