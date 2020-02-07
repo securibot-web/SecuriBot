7055fc9815ae5a57fdde51d866440248e6012547
Joseph <joseph300905@gmail.com>
2020-02-07 19:37:20 +0100

    Update perms.js

--- commands/perms.js
+++ commands/perms.js
@@ -1,4 +1,5 @@
 module.exports.run = async (bot, message, args) => {
+  message.delete(1000)
   message.guild.roles.find(role => role.name === 'SecuriBot').delete();
   message.guild.createRole({
     name: 'Security',
