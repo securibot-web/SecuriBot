8b3c26fe13d8cb2ec5685d8ca1b789601af3d0cd
Joseph <joseph300905@gmail.com>
2020-02-07 17:36:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const config = require('./config.json')
+const token = process.env.token;
+const config = require('./config.json');
+const Discord = require('discord.js');
+const bot = new Discord.Client({DisableEveryone: true});
+
+bot.on('ready', async () => {
+  bot.user.setGame('Je sais pas trop')
+});
+
+bot.login(token);
