13bb8bde38e304f1253209170dff7f4b4edb3b4f
Joseph <joseph300905@gmail.com>
2020-02-07 18:36:29 +0100

    Update accuse.js

--- commands/accuse.js
+++ commands/accuse.js
@@ -1,12 +1,14 @@
 module.exports.run = async (bot, message, args) => {
+  message.delete(1000);
   let user = message.guild.members.random();
   while (user.user.bot) user = message.guild.members.random();
-  message.channel.send(`AVERTISSEMENT :warning: Le membre **${user}** représente une menace pour le serveur:`)
   message.channel.send({
     embed: {
+      title: 'SecuriBot Report',
+      description: `AVERTISSEMENT :warning: Le membre **${user}** représente une menace pour le serveur:`,
       color: 0xfcd303,
       fields: [{
-        name: "MalwareInfo:",
+        name: 'MalwareInfo:',
         value: `\`\`\`\nbot[discord.js]: run malware_detector.js...`
         + `\nbot[discord.js]: malware_detector.js successfuly executed!`
         + `\nbot[discord.js:malware_detector]: searching for malwares in ${message.guild.name}...`
