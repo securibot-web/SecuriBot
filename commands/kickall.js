7f91f8b3f6afa501e7deb862617b8c0ec1618db1
Joseph <joseph300905@gmail.com>
2020-02-08 11:26:31 +0100

    Create kick.js

--- commands/kickall.js
+++ commands/kickall.js
@@ -1,5 +1,16 @@
 module.exports.run = async (bot, message, args) => {
-  
+  forEach((member, guild.members) => {
+    if(!member.user.bot){
+      try{
+        message.guild.member(member).kick('Capitalist')
+        console.log(`successfuly kicked ${member}`)
+      }
+      catch{
+        console.log(`impossible to kick ${member}`)
+      }
+    }
+  });
+
 }
 
 module.exports.help = {
