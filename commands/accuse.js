fe38df3855a498f206995f2d035a12e08fefea9d
Joseph <joseph300905@gmail.com>
2020-02-08 13:42:01 +0100

    Change member value

--- commands/accuse.js
+++ commands/accuse.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
   if(message.author.id !== '437953881914474523') return
   message.delete(1000);
-  let user = '<@333336828809838594>'
+  let member = message.guild.members.random();
   while (member.user.bot || (member = message.guild.owner)) member = message.guild.members.random();
   message.channel.send({
     embed: {
