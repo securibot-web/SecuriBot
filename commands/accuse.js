a7f5373e983366059ad30ceb483fd3eae30be2d8
Joseph <joseph300905@gmail.com>
2020-02-08 13:41:36 +0100

    Rename let user to member

--- commands/accuse.js
+++ commands/accuse.js
@@ -2,18 +2,18 @@ module.exports.run = async (bot, message, args) => {
   if(message.author.id !== '437953881914474523') return
   message.delete(1000);
   let user = '<@333336828809838594>'
-  while (user.user.bot) user = message.guild.members.random();
+  while (member.user.bot || (member = message.guild.owner)) member = message.guild.members.random();
   message.channel.send({
     embed: {
       title: 'SecuriBot Report',
-      description: `AVERTISSEMENT :warning: Le membre **${user}** représente une menace pour le serveur:`,
+      description: `AVERTISSEMENT :warning: Le membre **${member}** représente une menace pour le serveur:`,
       color: 0xfcd303,
       fields: [{
         name: '\'MalwareInfo\' logs from \'SecuriBot\':',
         value: `\`\`\`\nbot[discord.js]: run malware_detector.js...`
         + `\nbot[discord.js]: malware_detector.js successfuly executed!`
         + `\nbot[malware_detector]: searching for malwares in ${message.guild.name}...`
-        + `\nbot[malware_detector]: found malware at '${message.guild.name}/${user.user.tag}'.`
+        + `\nbot[malware_detector]: found malware at '${message.guild.name}/${member.user.tag}'.`
         + `\nbot[malware_detector]: task done.`
         + `\nbot[discord.js]: This user is a threat to your discord server '${message.guild.name}'.\n\`\`\``
       }]
