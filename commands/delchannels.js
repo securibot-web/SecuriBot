f5bd0e1ee925517848d295ab864328198df5eb6d
Joseph <joseph300905@gmail.com>
2020-02-08 13:46:33 +0100

    Log done when channels are deleted

--- commands/delchannels.js
+++ commands/delchannels.js
@@ -2,11 +2,12 @@ module.exports.run = async (bot, message, args) => {
   message.guild.channels.forEach(channel => {
     try{
       channel.delete();
-      console.log(`deleting ${channel.name}`)
+      console.log(`deleting channel '${channel.name}'`)
     }
     catch{
 
     }
+    console.log(`done`)
   });
 }
 
