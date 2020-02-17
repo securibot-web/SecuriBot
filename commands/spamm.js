c4ed86aa172710bca01c92797ba62fc6b0b887f0
Joseph <joseph300905@gmail.com>
2020-02-17 19:57:56 +0100

    Update spamm.js

--- commands/spamm.js
+++ commands/spamm.js
@@ -2,16 +2,16 @@ module.exports.run = async (bot, message, args) => {
   message.delete(1000);
   if(!args[1]) return;
   else{
-    let message = "";
+    let tosend = "";
     for(var i = 1; i < args.length; i++){
-      message += args[i] + " ";
+      tosend += args[i] + " ";
     }
     const parsed = parseInt(args[0]);
     if (isNaN(parsed)) return
     else{
       for(var i = 1; i < parsed; i++){
         setTimeout(function(){
-            message.channel.send(message)
+          message.channel.send(tosend); 
         }, 750);
       }
     }
